882e2d50705aab6a3c384a271444217d08b34e41
jullialexandria-collab <jullialexandria@gmail.com>
2026-09-13 05:29:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ menuBtn.addEventListener("click", () => {
     nav.classList.toggle("active");
 });
 
-
 async function loadArticles() {
     try {
         const response = await fetch("data/articles.json");
@@ -15,8 +14,31 @@ async function loadArticles() {
         }
 
         const articles = await response.json();
+        const newsGrid = document.getElementById("newsGrid");
 
-        console.log("Berita berhasil dimuat:", articles);
+        newsGrid.innerHTML = articles.map(article => `
+            <article class="news-card">
+
+                <div class="news-image">
+                    <span>${article.category}</span>
+                </div>
+
+                <div class="news-content">
+
+                    <small>${article.date}</small>
+
+                    <h3>${article.title}</h3>
+
+                    <p>${article.description}</p>
+
+                    <a href="${article.url}">
+                        Baca selengkapnya →
+                    </a>
+
+                </div>
+
+            </article>
+        `).join("");
 
     } catch (error) {
         console.error("Terjadi kesalahan:", error);
