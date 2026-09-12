70ab99f8b5ac8f29a61483e1477c520a71989ef6
jullialexandria-collab <jullialexandria@gmail.com>
2026-09-13 05:11:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ const nav = document.querySelector(".nav");
 menuBtn.addEventListener("click", () => {
     nav.classList.toggle("active");
 });
+
+
+async function loadArticles() {
+    try {
+        const response = await fetch("data/articles.json");
+
+        if (!response.ok) {
+            throw new Error("Gagal mengambil data berita");
+        }
+
+        const articles = await response.json();
+
+        console.log("Berita berhasil dimuat:", articles);
+
+    } catch (error) {
+        console.error("Terjadi kesalahan:", error);
+    }
+}
+
+loadArticles();
